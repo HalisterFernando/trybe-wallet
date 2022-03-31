d6560f5c8c0dc16bd1a40510bd6300cfd8c3e4f5
Seu nome <halisterfernando@hotmail.com>
2022-03-31 19:12:47 -0300

    requisito 1 finalizado

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+export const EMAIL = 'EMAIL';
+
+export const userEmail = (email) => ({
+  type: EMAIL,
+  email,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,90 @@
+import { PropTypes } from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { userEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      isDisabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({ [name]: value }, () => this.inputValidation());
+  }
+
+  inputValidation = () => {
+    const { email, password } = this.state;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const validateEmail = emailRegex.test(email);
+
+    const minLength = 6;
+    const validatePassword = password.length >= minLength;
+
+    this.setState({ isDisabled: !(validateEmail && validatePassword) });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isDisabled } = this.state;
+    const { saveEmail } = this.props;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          <label htmlFor="email">
+            <input
+              data-testid="email-input"
+              type="text"
+              name="email"
+              id="email"
+              value={ email }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <br />
+          <label htmlFor="password">
+            <input
+              data-testid="password-input"
+              type="password"
+              name="password"
+              id="password"
+              value={ password }
+              onChange={ this.handleChange }
+
+            />
+
+          </label>
+          <br />
+          <Link to="/carteira">
+            <button
+              type="button"
+              disabled={ isDisabled }
+              onClick={ () => saveEmail(email) }
+            >
+              Entrar
+            </button>
+          </Link>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  saveEmail: (email) => dispatch(userEmail(email)),
+});
+
+Login.propTypes = {
+  saveEmail: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
